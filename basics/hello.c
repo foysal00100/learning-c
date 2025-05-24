196ab62a0dcc154114cc44527b843c2876caa02a
Foysal Ahmed Rafy <foysal00100@gmail.com>
2025-05-24 23:37:47 +0600

    added more message inside hello.c fie

--- basics/hello.c
+++ basics/hello.c
@@ -1,2 +1,5 @@
 #include <stdio.h>
-int main() {printf("HELLO, WORLD! \n"); return 0;}
+int main() {printf("HELLO, WORLD! \n"); 
+    printf("This is a C program.\n");
+    printf("Spread love.\n");
+    return 0;}
